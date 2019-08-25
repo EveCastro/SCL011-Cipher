a761b026a2c636aae0653ad88eacb4323139ca6f
EveCastro <eve.cdesign5@gmail.com>
2019-08-25 00:43:10 -0400

    Subiendo archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,59 @@
 window.cipher = {
-  encode: () => {
+  encode: (mensaje,offset) => {
     /* Acá va tu código */
+   
+    //variable se declara vacia para agregarle un valor
+    let mensajefinal= "";
+
+    
+      //para recorrer el texto captado
+    for (let i = 0; i<mensaje.length; i++) {
+
+      
+      let cifrado = (mensaje.charCodeAt(i));
+      if(cifrado >=65 && cifrado<=90){
+        mensajefinal+=String.fromCharCode(((cifrado-65 +offset)% 26)+65);
+      }
+      //para crear el espacio
+     else if (cifrado ==32){
+       mensajefinal+=String.fromCharCode(cifrado)
+     }
+    
+      //mostrar el mensaje cifrado en el box 2
+     //document.getElementById ("mensaje2").value=mensajefinal;
+    
+    }
+     return mensajefinal;
     
   },
-  decode: () => {
+
+
+
+
+  decode: (mensaje2,offset) => {
     /* Acá va tu código */
+    //variable se declara vacia para agregarle un valor
+    let mensajefinal2= "";
+
+    for (let i = 0; i<mensaje2.length; i++) {
+
+      
+      let descifrado = (mensaje2.charCodeAt(i));
+      if(descifrado >=65 && descifrado<=90){
+        mensajefinal2+=String.fromCharCode(((descifrado+65 -offset)% 26)+65);
+        }
+     else if (descifrado ==32){
+       mensajefinal2+=String.fromCharCode(descifrado)
+     }
+     
+      //document.getElementById ("mensaje2").value=mensajefinal2;
+    
+    }  
+    
+
+     return mensajefinal2;
   }
+   
+  
+
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 /*Acá va tu código*/
+
+const cifrar= document.getElementById("cifrar"); 
+let mensajeCifrado=document.getElementById("respuestaCifrado");
+cifrar.addEventListener("click",()=> {
+let mensaje = document.getElementById ("mensaje").value.toUpperCase();
+let offset = parseInt(document.getElementById ("offset").value);
+mensajeCifrado.innerHTML=window.cipher.encode(mensaje,offset);
+
+});
+
+const descifrar=document.getElementById("descifrar");
+let mensajeDescifrado=document.getElementById("respuestaDescifrado");
+descifrar.addEventListener("click",()=> {
+let mensaje2 = document.getElementById ("mensaje2").value.toUpperCase(); 
+let offset = parseInt(document.getElementById ("offset").value);
+mensajeDescifrado.innerHTML=window.cipher.decode(mensaje2,offset);
+});
+
+
+
+
+
+
+
+
